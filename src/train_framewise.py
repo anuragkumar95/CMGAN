007b8e6c2d83c45388f13ccabfc3f5519c3e5daa
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-09-29 16:36:29 -0400

    .

--- src/train_framewise.py
+++ src/train_framewise.py
@@ -61,17 +61,9 @@ class Trainer:
                             num_features=self.n_fft // 2 + 1, 
                             win_len=self.win_len+1, 
                             gpu_id=gpu_id)
-        #summary(
-        #    self.model, [(1, 2, args.cut_len // self.hop + 1, int(self.n_fft / 2) + 1)]
-        #)
+      
         self.discriminator = discriminator.Discriminator(ndf=16)
-        #summary(
-        #    self.discriminator,
-        #    [
-        #        (1, 1, int(self.n_fft / 2) + 1, args.cut_len // self.hop + 1),
-        #        (1, 1, int(self.n_fft / 2) + 1, args.cut_len // self.hop + 1),
-        #    ],
-        #)
+       
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=args.init_lr)
         self.optimizer_disc = torch.optim.AdamW(
             self.discriminator.parameters(), lr=2 * args.init_lr
@@ -91,7 +83,7 @@ class Trainer:
         noisy, clean = torch.transpose(noisy * c, 0, 1), torch.transpose(
             clean * c, 0, 1
         )
-        #NOTE: PLEASE REMOVE COMMENTS WHEN TRAINING ON GPU
+
         win = torch.hamming_window(self.n_fft)
         if self.gpu_id is not None:
             win = win.to(self.gpu_id)
@@ -111,39 +103,9 @@ class Trainer:
             onesided=True,
         )
         return noisy_spec, clean_spec
-        
-    def forward_generator_step(self, clean_spec, noisy_spec):
-        noisy_spec = power_compress(noisy_spec).permute(0, 1, 3, 2)
-        clean_spec = power_compress(clean_spec)
-        clean_real = clean_spec[:, 0, :, :].unsqueeze(1)
-        clean_imag = clean_spec[:, 1, :, :].unsqueeze(1)
-
-        est_real, est_imag = self.model(noisy_spec)
-        est_real, est_imag = est_real.permute(0, 1, 3, 2), est_imag.permute(0, 1, 3, 2)
-        est_mag = torch.sqrt(est_real**2 + est_imag**2)
-        clean_mag = torch.sqrt(clean_real**2 + clean_imag**2)
-
-        est_spec_uncompress = power_uncompress(est_real, est_imag).squeeze(1)
-        est_audio = torch.istft(
-            est_spec_uncompress,
-            self.n_fft,
-            self.hop,
-            window=torch.hamming_window(self.n_fft),#.to(self.gpu_id),
-            onesided=True,
-        )
-
-        return {
-            "est_real": est_real,
-            "est_imag": est_imag,
-            "est_mag": est_mag,
-            "clean_real": clean_real,
-            "clean_imag": clean_imag,
-            "clean_mag": clean_mag,
-            "est_audio": est_audio,
-        }
     
-    def forward_generator_step2(self, noisy_stack):#, clean_real_stack, clean_imag_stack, clean):
-        steps, b, _, _, f = noisy_stack.shape
+    def forward_generator_step2(self, noisy_stack, clean_real_stack, clean_imag_stack, clean):
+        steps, b, _, win_len, f = noisy_stack.shape
         samples = self.samples
         for idx in range(steps):
             mini_batch = noisy_stack[idx, :, :, :, :]
@@ -151,7 +113,7 @@ class Trainer:
             est_real, est_imag = self.model(mini_batch, k=samples)
             est_real, est_imag = est_real.permute(0, 1, 2, 4, 3), est_imag.permute(0, 1, 2, 4, 3)
             est_mag = torch.sqrt(est_real**2 + est_imag**2)
-            #clean_mag = torch.sqrt(clean_real_stack[idx, :, :, :, (win_len//2) + 1]**2 + clean_imag_stack[idx, :, :, :, (win_len//2) + 1]**2).unsqueeze(-1)
+            clean_mag = torch.sqrt(clean_real_stack[idx, :, :, :, (win_len//2) + 1]**2 + clean_imag_stack[idx, :, :, :, (win_len//2) + 1]**2).unsqueeze(-1)
 
             est_audios = []
             for k in range(samples):
@@ -177,25 +139,25 @@ class Trainer:
             
             est_audios = torch.stack(est_audios, dim=0)
             #clip out audio with the sametime index as in the current frame
-            #st = idx * self.hop
-            #en = st + self.n_fft
+            st = idx * self.hop
+            en = st + self.n_fft
             #Account for zero pading
-            #aud_pad = torch.zeros(b, self.n_fft)
-            #if self.gpu_id is not None:
-            #    aud_pad = aud_pad.to(self.gpu_id)
-            #clean_aud = torch.cat([aud_pad, clean[:, st:en], aud_pad], dim=-1)
+            aud_pad = torch.zeros(b, self.n_fft)
+            if self.gpu_id is not None:
+                aud_pad = aud_pad.to(self.gpu_id)
+            clean_aud = torch.cat([aud_pad, clean[:, st:en], aud_pad], dim=-1)
             
-            #clean_real_slc = clean_real_stack[idx, :, :, :, (win_len//2)+1].unsqueeze(-1)
-            #clean_imag_slc = clean_imag_stack[idx, :, :, :, (win_len//2)+1].unsqueeze(-1)
+            clean_real_slc = clean_real_stack[idx, :, :, :, (win_len//2)+1].unsqueeze(-1)
+            clean_imag_slc = clean_imag_stack[idx, :, :, :, (win_len//2)+1].unsqueeze(-1)
 
             yield  {"est_real": est_real,
                     "est_imag": est_imag,
                     "est_mag": est_mag,
-                    #"clean_real": clean_real_slc,
-                    #"clean_imag": clean_imag_slc,
-                    #"clean_mag": clean_mag,
+                    "clean_real": clean_real_slc,
+                    "clean_imag": clean_imag_slc,
+                    "clean_mag": clean_mag,
                     "est_audio": est_audios,
-                    #"clean": clean_aud
+                    "clean": clean_aud
                     }
     
 
@@ -321,10 +283,10 @@ class Trainer:
                 if self.gpu_id is not None:
                     pad = pad.to(self.gpu_id)
                 n_frame = noisy_spec[:,:, :i + (win_len//2)+1,:]
-                #c_frame = clean_spec[:,:, :, :i + (win_len//2)+1]
+                c_frame = clean_spec[:,:, :, :i + (win_len//2)+1]
                 n_frame = torch.cat([pad, n_frame], dim=2)
-                #pad = pad.permute(0, 1, 3, 2)
-                #c_frame = torch.cat([pad, c_frame], dim=-1)
+                pad = pad.permute(0, 1, 3, 2)
+                c_frame = torch.cat([pad, c_frame], dim=-1)
                 
             elif i > time - 1 - win_len:
                 pad_len = win_len - (time - 1 - i)
@@ -332,18 +294,18 @@ class Trainer:
                 if self.gpu_id is not None:
                     pad = pad.to(self.gpu_id)
                 n_frame = noisy_spec[:, :, i:, :]
-                #c_frame = clean_spec[:, :, :, i:]
+                c_frame = clean_spec[:, :, :, i:]
                 n_frame = torch.cat([n_frame, pad], dim=2)
-                #pad = pad.permute(0, 1, 3, 2)
-                #c_frame = torch.cat([c_frame, pad], dim=-1)
+                pad = pad.permute(0, 1, 3, 2)
+                c_frame = torch.cat([c_frame, pad], dim=-1)
                   
             else:
                 n_frame = noisy_spec[:, :, i:i + win_len+1, :]
-                #c_frame = clean_spec[:, :, :, i:i + win_len+1]
+                c_frame = clean_spec[:, :, :, i:i + win_len+1]
             
             noisy_win_stack = torch.cat([noisy_win_stack, n_frame.unsqueeze(0)], dim=0)
-            #clean_win_real_stack = torch.cat([clean_win_real_stack, c_frame[:, 0, :, :].unsqueeze(1).unsqueeze(0)], dim=0)
-            #clean_win_imag_stack = torch.cat([clean_win_imag_stack, c_frame[:, 1, :, :].unsqueeze(1).unsqueeze(0)], dim=0)
+            clean_win_real_stack = torch.cat([clean_win_real_stack, c_frame[:, 0, :, :].unsqueeze(1).unsqueeze(0)], dim=0)
+            clean_win_imag_stack = torch.cat([clean_win_imag_stack, c_frame[:, 1, :, :].unsqueeze(1).unsqueeze(0)], dim=0)
 
         #Perhaps append to a list and concatenate once will be more optimal?
         generator_outputs = {
@@ -356,68 +318,39 @@ class Trainer:
             "est_audio": []
         }
 
-        outputs = {
-            "est_real": [],
-            "est_imag": [],
-            "est_mag": [],
-            "clean_real": [],
-            "clean_imag": [],
-            "clean_mag": [],
-            "est_audio": []
-        }
-
         loss_step=20
 
         #Calculate generator loss over window frames and collect outputs. 
-        for i, output in enumerate(self.forward_generator_step2(noisy_win_stack[1:, :, :, :, :])): 
-                                                #clean_win_real_stack[1:, :, :, :, :], 
-                                                #clean_win_imag_stack[1:, :, :, :, :],
-                                                #clean)):
+        for i, outputs in enumerate(self.forward_generator_step2(noisy_win_stack[1:, :, :, :, :], 
+                                                clean_win_real_stack[1:, :, :, :, :], 
+                                                clean_win_imag_stack[1:, :, :, :, :],
+                                                clean)):
            
-            output["one_labels"] = torch.ones(args.batch_size)
+            outputs["one_labels"] = torch.ones(args.batch_size)
             if self.gpu_id is not None:
-                output["one_labels"] =  output["one_labels"].to(self.gpu_id)
+                outputs["one_labels"] =  outputs["one_labels"].to(self.gpu_id)
             
             #Store the generator outputs and pass it to the discriminator
-            outputs['est_real'].append(output['est_real'])
-            outputs['est_imag'].append(output['est_imag'])
-            outputs['est_mag'].append(output['est_mag'])
-            outputs['est_audio'].append(output['est_audio'][..., self.n_fft:-self.n_fft])
+            generator_outputs['est_real'].append(outputs['est_real'])
+            generator_outputs['est_imag'].append(outputs['est_imag'])
+            generator_outputs['est_mag'].append(outputs['est_mag'])
 
             if (i+1) % loss_step == 1:
                 st = (i+1) // loss_step
                 en = min(st+20, clean_spec.shape[3])
 
-                outputs['est_real'] = torch.stack(generator_outputs['est_real'], dim=3).squeeze(4)
-                outputs['est_imag'] = torch.stack(generator_outputs['est_imag'], dim=2).squeeze(4)
-                outputs['est_mag'] = torch.stack(generator_outputs['est_mag'], dim=3).squeeze(4)
-                outputs['clean_real'] = clean_spec[:, 0, st:en, :].unsqueeze(1)
-                outputs['clean_imag'] = clean_spec[:, 1, st:en, :].unsqueeze(1)
-                outputs['clean_mag'] = torch.sqrt(clean_spec[:, 0, st:en, :]**2 + clean_spec[:, 1, st:en, :]**2).unsqueeze(1)
-                outputs['est_audio'] = torch.stack(outputs['est_audio'], dim=-1)
-                outputs['clean'] = clean[:, st * self.hop: (st * self.hop) + (loss_step * self.n_fft)]
+                generator_outputs['est_real'] = torch.stack(generator_outputs['est_real'], dim=3).squeeze(4)
+                generator_outputs['est_imag'] = torch.stack(generator_outputs['est_imag'], dim=2).squeeze(4)
+                generator_outputs['est_mag'] = torch.stack(generator_outputs['est_mag'], dim=3).squeeze(4)
+                generator_outputs['clean_real'] = clean_spec[:, 0, st:en, :].unsqueeze(1)
+                generator_outputs['clean_imag'] = clean_spec[:, 1, st:en, :].unsqueeze(1)
+                generator_outputs['clean_mag'] = torch.sqrt(clean_spec[:, 0, st:en, :]**2 + clean_spec[:, 1, st:en, :]**2).unsqueeze(1)
 
-
-                loss = self.calculate_generator_loss2(outputs, samples=self.samples)
+                loss = self.calculate_generator_loss2(generator_outputs, samples=self.samples)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                print(f"Generator loop step:{i}, Loss:{loss}") 
-
-                outputs = {
-                            "est_real": [],
-                            "est_imag": [],
-                            "est_mag": [],
-                            "clean_real": [],
-                            "clean_imag": [],
-                            "clean_mag": [],
-                            "est_audio": []
-                        }
-            
-            generator_outputs['est_real'].append(output['est_real'].mean(0))
-            generator_outputs['est_imag'].append(output['est_imag'].mean(0))
-            generator_outputs['est_mag'].append(output['est_mag'].mean(0))
-            #generator_outputs['est_audio'].append(output['est_audio'].mean(0)[..., self.n_fft:-self.n_fft])
+                print(f"Generator loop step:{i}, Loss:{loss}, RAM:{psutil.vistual_memory().percent}") 
             
         generator_outputs['est_real'] = torch.stack(generator_outputs['est_real'], dim=2).squeeze(3)
         generator_outputs['est_imag'] = torch.stack(generator_outputs['est_imag'], dim=2).squeeze(3)
@@ -427,28 +360,29 @@ class Trainer:
         generator_outputs['clean_mag'] = torch.sqrt(clean_spec[:, 0, :, :]**2 + clean_spec[:, 1, :, :]**2).unsqueeze(1)
         
         est_spec_uncompress = power_uncompress(generator_outputs['est_real'], 
-                                               generator_outputs['est_imag']).squeeze(1)
+                                               generator_outputs['est_imag']).squeeze(1).squeeze(2)
         
         win = torch.hamming_window(self.n_fft)
         if self.gpu_id is not None:
             win = win.to(self.gpu_id)
         
-        est_audio = torch.istft(
-                        est_spec_uncompress.permute(0,2,1,3),
+        est_auds = []
+        for spec in est_spec_uncompress:
+            est_audio = torch.istft(
+                        spec.permute(0,2,1,3),
                         self.n_fft,
                         self.hop,
                         window=win,
                         onesided=True,
                     )
+            est_auds.append(est_audio)
+        est_auds = torch.stack(est_auds, dim=0)
+        
         generator_outputs['est_audio'] = est_audio
         generator_outputs['clean'] = clean
         generator_outputs['one_labels'] = one_labels
 
-        loss = self.calculate_generator_loss(generator_outputs)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
+        l
         print(f"Generator_loss:{loss}")
 
         # Train Discriminator
@@ -463,45 +397,6 @@ class Trainer:
 
     
         yield loss.item(), discrim_loss_metric.item(), pesq_score.mean().item()
-
-
-    def train_step(self, batch):
-        # Trainer generator
-        clean = batch[0]
-        noisy = batch[1]
-        one_labels = torch.ones(args.batch_size)
-        if self.gpu_id:
-            clean = batch[0].to(self.gpu_id)
-            noisy = batch[1].to(self.gpu_id)
-            one_labels = torch.ones(args.batch_size).to(self.gpu_id)
-        
-        noisy_spec, clean_spec = self.create_spectrograms(noisy, clean)
-        noisy_spec = power_compress(noisy_spec).permute(0, 1, 3, 2)
-        clean_spec = power_compress(clean_spec)
-
-        generator_outputs = self.forward_generator_step(
-            clean_spec,
-            noisy_spec,
-        )
-        generator_outputs["one_labels"] = one_labels
-        generator_outputs["clean"] = clean
-
-        loss = self.calculate_generator_loss(generator_outputs)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-        # Train Discriminator
-        discrim_loss_metric = self.calculate_discriminator_loss(generator_outputs)
-
-        if discrim_loss_metric is not None:
-            self.optimizer_disc.zero_grad()
-            discrim_loss_metric.backward()
-            self.optimizer_disc.step()
-        else:
-            discrim_loss_metric = torch.tensor([0.0])
-
-        return loss.item(), discrim_loss_metric.item()
 
     @torch.no_grad()
     def test_step2(self, batch):
@@ -622,29 +517,6 @@ class Trainer:
 
         return loss.item(), discrim_loss_metric.item(), pesq_score.mean().item()
 
-
-    @torch.no_grad()
-    def test_step(self, batch):
-
-        clean = batch[0].to(self.gpu_id)
-        noisy = batch[1].to(self.gpu_id)
-        one_labels = torch.ones(args.batch_size).to(self.gpu_id)
-
-        generator_outputs = self.forward_generator_step(
-            clean,
-            noisy,
-        )
-        generator_outputs["one_labels"] = one_labels
-        generator_outputs["clean"] = clean
-
-        loss = self.calculate_generator_loss(generator_outputs)
-
-        discrim_loss_metric = self.calculate_discriminator_loss(generator_outputs)
-        if discrim_loss_metric is None:
-            discrim_loss_metric = torch.tensor([0.0])
-
-        return loss.item(), discrim_loss_metric.item()
-
     def test2(self):
         self.model.eval()
         self.discriminator.eval()
@@ -722,37 +594,6 @@ class Trainer:
         logging.info(template.format(self.gpu_id, gen_loss_avg, disc_loss_avg))
 
         return gen_loss_avg
-
-    def train(self):
-        scheduler_G = torch.optim.lr_scheduler.StepLR(
-            self.optimizer, step_size=args.decay_epoch, gamma=0.5
-        )
-        scheduler_D = torch.optim.lr_scheduler.StepLR(
-            self.optimizer_disc, step_size=args.decay_epoch, gamma=0.5
-        )
-        for epoch in range(args.epochs):
-            self.model.train()
-            self.discriminator.train()
-            for idx, batch in enumerate(self.train_ds):
-                step = idx + 1
-                loss, disc_loss = self.train_step(batch)
-                template = "GPU: {}, Epoch {}, Step {}, loss: {}, disc_loss: {}"
-                if (step % args.log_interval) == 0:
-                    logging.info(
-                        template.format(self.gpu_id, epoch, step, loss, disc_loss)
-                    )
-            gen_loss = self.test2()
-            path = os.path.join(
-                args.save_model_dir,
-                "CMGAN_epoch_" + str(epoch) + "_" + str(gen_loss)[:5],
-            )
-            if not os.path.exists(args.save_model_dir):
-                os.makedirs(args.save_model_dir)
-            if self.gpu_id == 0:
-                torch.save(self.model.module.state_dict(), path)
-            scheduler_G.step()
-            scheduler_D.step()
-
 
 def main(rank: int, world_size: int, args):
     
